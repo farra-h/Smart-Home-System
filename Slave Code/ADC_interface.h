f087faba17581eb93bb8342147c99003e24a110f
Farah Mahmoud <134429241+farra-h@users.noreply.github.com>
2024-08-01 12:35:55 +0300

    Add files via upload

---
+++ Slave Code/ADC_interface.h
@@ -0,0 +1,30 @@
+﻿/*
+ * ADC_interface.h
+ *
+ *  Author: FaRaH
+ */ 
+
+
+#ifndef ADC_INTERFACE_H_
+#define ADC_INTERFACE_H_
+
+#include "STD_TYPES.h"
+
+/*
+	Function Name        : ADC_vinit
+	Function Returns     : void
+	Function Arguments   : void
+	Function Description : Initialize the ADC.
+*/
+void ADC_vinit(void);
+
+/*
+	Function Name        : ADC_u16Read
+	Function Returns     : uint16
+	Function Arguments   : void
+	Function Description : Read the value which converted by the ADC.
+*/
+uint16 ADC_u16Read(void);
+
+
+#endif /* ADC_INTERFACE_H_ */
---
+++ Slave Code/Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+ADC_program.c
+
+DIO_program.c
+
+LED_program.c
+
+main.c
+
+SPI_program.c
+
+TIMER_program.c
+
